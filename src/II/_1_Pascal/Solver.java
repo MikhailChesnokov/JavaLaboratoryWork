f921597fe078140ea602a1da6f4246a9f75ab7b9
Михаил <mishachesnokov111@gmail.com>
2017-10-26 15:11:20 +0500

    Initial Pascal triangle

---
+++ src/II/_1_Pascal/Solver.java
@@ -0,0 +1,23 @@
+package II._1_Pascal;
+
+import javafx.util.Pair;
+import java.util.HashMap;
+
+public class Solver {
+
+    private static HashMap<Pair<Integer,Integer>, Integer> combinationsCache = new HashMap<>();
+
+    public static void main() {
+        for (int i = 1; i <= 6; i++,System.out.println())
+            for (int j = 1; j <= i; j++)
+                System.out.print(combinations(new Pair(i,j)) + " ");
+    }
+
+    public static int combinations(Pair<Integer, Integer> pair) {
+        if (pair.getKey() < 2 || pair.getValue() < 2 || pair.getKey() <= pair.getValue()) return 1;
+        if (combinationsCache.containsKey(pair)) return combinationsCache.get(pair);
+        combinationsCache.put(pair, combinations(new Pair<>(pair.getKey() - 1, pair.getValue() - 1)) +
+                                    combinations(new Pair<>(pair.getKey() - 1, pair.getValue())));
+        return combinationsCache.get(pair);
+    }
+}
